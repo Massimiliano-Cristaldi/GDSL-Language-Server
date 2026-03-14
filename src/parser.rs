018b5f1f50c060c4c6802eddfceeb9dc46fd63b5
bad_lvck <cristaldi.massimiliano@gmail.com>
2026-03-14 15:54:34 +0100

    context enum test

--- src/parser.rs
+++ src/parser.rs
@@ -1,5 +1,7 @@
+use core::panic;
 use std::collections::HashMap;
 use std::sync::LazyLock;
+use std::task::Context;
 
 use crate::common::{DataType, Function, Token, TokenKind};
 use crate::tables::{BUILT_IN_FUNCTIONS, GLOBALS, TYPE_KEYWORDS};
@@ -8,8 +10,8 @@ pub struct Parser<'a> {
     tokens: Vec<Token<'a>>,
     index: usize,
     scope: Scope,
-    context: PContext,
-    subcontext: u8,
+    curr_context: PContext,
+    prev_contexts: Vec<PContext>,
     idents: HashMap<&'a str, DataType>,
     functions: HashMap<&'a str, Function>,
     diagnostics: Vec<Diagnostic>,
@@ -21,14 +23,75 @@ enum Scope {
     FnBody,
 }
 
-#[derive(Clone, Copy, PartialEq)]
-enum PContext {
+#[derive(Clone, PartialEq)]
+enum PContextKind {
     Default,
     IdentDecl,
-    FnDecl,
-    FnCall,
-    Operation,
+    FnDecl {
+        args: Vec<DataType>,
+    },
+    FnCall {
+        args: Vec<DataType>,
+    },
+    Operation {
+        lhs: Option<DataType>,
+        operator: &'static str,
+        rhs: DataType,
+    },
     Cast,
+}
+
+#[derive(Clone, PartialEq)]
+struct PContext {
+    kind: PContextKind,
+    subcontext: usize
+}
+
+impl PContext {
+    fn new_default() -> PContext {
+        return PContext { kind: PContextKind::Default, subcontext: 0 }
+    }
+    
+    fn new_ident_decl() -> PContext {
+        return PContext { kind: PContextKind::IdentDecl, subcontext: 0 };
+    }
+
+    fn new_fn_decl() -> PContext {
+        return PContext { kind: PContextKind::FnDecl { args: vec![] }, subcontext: 0 };
+    }
+
+    fn new_fn_call() -> PContext {
+        return PContext { kind: PContextKind::FnCall { args: vec![] }, subcontext: 0 };
+    }
+
+    fn new_operation() -> PContext {
+        return PContext { kind: PContextKind::Operation { lhs: None, operator: "", rhs: DataType::Unknown }, subcontext: 0 };
+    }
+
+    fn new_cast() -> PContext {
+        return PContext { kind: PContextKind::Cast, subcontext: 0 };
+    }
+
+    fn get_arg(&self, index: usize) -> DataType {
+        return match &self.kind {
+            PContextKind::FnCall { args } | PContextKind::FnDecl { args }=> {
+                *args.get(index).expect(&format!("Tried to access non existent function argument {index}"))
+            },
+            _ => panic!("Cannot get function signature data from non-function contexts")
+        }
+    }
+
+    fn push_arg(&mut self, arg: DataType) -> () {
+        return match &mut self.kind {
+            PContextKind::FnCall { args } => {
+                args.push(arg)
+            },
+            PContextKind::FnDecl { args } => {
+                args.push(arg)
+            },
+            _ => panic!("Cannot modify function signature data in non-function contexts")
+        }
+    }
 }
 
 #[derive(Debug, PartialEq)]
@@ -68,8 +131,8 @@ impl<'a> Parser<'a> {
             tokens: tokens,
             index: 0,
             scope: Scope::Global,
-            context: PContext::Default,
-            subcontext: 0,
+            curr_context: PContext::new_default(),
+            prev_contexts: Vec::with_capacity(256),
             idents: HashMap::new(),
             functions: HashMap::new(),
             diagnostics: Vec::with_capacity(256),
@@ -77,36 +140,40 @@ impl<'a> Parser<'a> {
     }
 
     pub fn get_diagnostics(mut self) -> Vec<Diagnostic> {
-        while let Some(curr) = self.next() {
-            if curr.kind == TokenKind::Comment {
+        while let Some(token) = self.next() {
+            //TODO: check if comment is at end of line
+            if token.kind == TokenKind::Comment {
                 continue;
             }
             
-            match self.context {
-                PContext::IdentDecl => {
-                    self.ident_decl_branch(curr);
+            match self.curr_context.kind {
+                PContextKind::IdentDecl { .. } => {
+                    self.ident_decl_branch(token);
                 }
-                PContext::FnDecl => {
-                    self.fn_decl_branch(curr);
+                PContextKind::FnDecl { .. } => {
+                    self.fn_decl_branch(token);
                 }
-                PContext::FnCall => {}
-                PContext::Operation => {}
-                PContext::Cast => {}
-                PContext::Default => match (curr.kind, curr.value) {
-                    (TokenKind::TypeKeyword, _)
-                    | (TokenKind::MiscKeyword, "uniform")
-                    | (TokenKind::MiscKeyword, "const") => self.context = PContext::IdentDecl,
+                PContextKind::FnCall { .. } => {
+                    self.fn_call_branch(token);
+                }
+                PContextKind::Operation { .. } => {}
+                PContextKind::Cast { .. } => {}
+                PContextKind::Default => match (token.kind, token.value) {
+                    (TokenKind::TypeKeyword, _) |
+                    (TokenKind::MiscKeyword, "uniform") |
+                    (TokenKind::MiscKeyword, "const") => self.push_context(PContext::new_ident_decl()),
                     _ => {}
                 },
             }
 
             //TODO: check if function declarations can be nested in GDSL
-            if self.scope == Scope::FnBody && curr.value == "}" {
+            if self.scope == Scope::FnBody && token.value == "}" {
                 self.scope = Scope::Global;
             }
 
-            if self.can_recover(curr) {
-                self.reset_context();
+            //TODO: keep track of poisoned state
+            if self.can_recover(token) {
+                self.pop_context();
             }
         }
 
@@ -123,9 +190,16 @@ impl<'a> Parser<'a> {
         return self.tokens.get(index);
     }
 
-    fn reset_context(&mut self) -> () {
-        self.context = PContext::Default;
-        self.subcontext = 0;
+    fn push_context(&mut self, new_context: PContext) -> () {
+        self.prev_contexts.push(self.curr_context.clone());
+        self.curr_context = new_context;
+    } 
+    
+    fn pop_context(&mut self) -> () {
+        self.curr_context = 
+            self.prev_contexts
+            .pop()
+            .unwrap_or(PContext::new_default());
     }
 
     fn resolve_fn_type(&self, fn_name: &str) -> TokenKind {
@@ -150,14 +224,16 @@ impl<'a> Parser<'a> {
         }
     }
 
-    fn expect_kind(&mut self, token: Token, kind: TokenKind) -> () {
+    fn expect_kind(&mut self, token: Token, kind: TokenKind) -> bool {
         let token_kind = if token.is_fn() {
             self.resolve_fn_type(token.value)
         } else {
             token.kind
         };
         
-        if token_kind != kind {
+        if token_kind == kind {
+            return true;
+        } else {
             self.push_diagnostic(
                 token,
                 format!(
@@ -165,6 +241,8 @@ impl<'a> Parser<'a> {
                     kind, token.kind
                 ),
             );
+
+            return false;
         }
     }
 
@@ -209,8 +287,8 @@ impl<'a> Parser<'a> {
         }
     }
 
-    fn ident_decl_branch(&mut self, mut token: Token<'a>) -> () {
-        match self.subcontext {
+    fn ident_decl_branch(&mut self, token: Token<'a>) -> () {
+        match self.curr_context.subcontext {
             0 => {
                 self.expect_kind(token, TokenKind::Ident(DataType::Unknown));
                 //TODO: understand this error
@@ -218,19 +296,20 @@ impl<'a> Parser<'a> {
                 //     Some(t) if ["uniform", "const"].contains(&t.value) => true,
                 //     _ => false,
                 // };
-                self.subcontext += 1;
+                self.curr_context.subcontext += 1;
             }
             1 => {
                 if token.value == "(" {
-                    self.context = PContext::FnDecl;
-                    self.subcontext = 0;
+                    self.push_context(PContext::new_fn_decl());
+                    self.curr_context.subcontext = 0;
                     return;
                 }
 
                 self.expect_value(token, "=");
-                self.subcontext += 1;
+                self.curr_context.subcontext += 1;
             }
             2 => {
+                //TODO: keep track of var_type as we iterate rather than peeking back
                 let var_type: DataType = match self.peek_back(3) {
                     Some(token) if token.kind == TokenKind::TypeKeyword => {
                         *TYPE_KEYWORDS.get(token.value).unwrap_or_else(|| {
@@ -256,67 +335,82 @@ impl<'a> Parser<'a> {
                     DataType::Unknown => {
                         self.push_diagnostic(token, String::from("Use of ident before assignment"));
                     }
-                    data_type => self.expect_kind(token, TokenKind::Ident(data_type)),
+                    data_type => {
+                        self.expect_kind(token, TokenKind::Ident(data_type));
+                    }
                 }
 
                 if token.is_fn() {
-                    self.context = PContext::FnCall;
-                    self.subcontext = 0;
+                    self.push_context(PContext::new_fn_call());
                 } else {
-                    self.subcontext += 1;
+                    self.curr_context.subcontext += 1;
                 }
             }
             3 => {
                 //TODO handle expressions before semicolon
                 self.expect_value(token, ";");
-                self.subcontext += 1;
+                self.curr_context.subcontext += 1;
             }
             _ => {
-                self.reset_context();
+                self.pop_context();
             }
         }
     }
 
     fn fn_decl_branch(&mut self, token: Token) -> () {
-        match self.subcontext {
+        match self.curr_context.subcontext {
             0 => {
                 if token.value == ")" {
-                    self.subcontext = 3;
+                    self.curr_context.subcontext = 3;
                     return;
                 }
 
-                self.expect_kind(token, TokenKind::TypeKeyword);
-                self.subcontext += 1;
+                if self.expect_kind(token, TokenKind::TypeKeyword) {
+                    self.curr_context.push_arg(*TYPE_KEYWORDS.get(token.value).unwrap());
+                }
+
+                self.curr_context.subcontext += 1;
             }
             1 => {
                 self.expect_kind(token, TokenKind::Ident(DataType::Unknown));
-                self.subcontext += 1;
+                self.curr_context.subcontext += 1;
             }
             2 => {
                 if token.value == "," {
-                    self.subcontext = 0;
+                    self.curr_context.subcontext = 0;
                     return;
                 }
 
                 self.expect_value(token, ")");
-                self.subcontext += 1;
+                self.curr_context.subcontext += 1;
             }
             3 => {
                 self.expect_value(token, "{");
                 self.scope = Scope::FnBody;
 
-                self.reset_context();
+                self.pop_context();
             }
             _ => {
-                self.reset_context();
+                self.pop_context();
+            }
+        }
+    }
+
+    fn fn_call_branch(&mut self, token: Token) -> () {
+        match self.curr_context.subcontext {
+            0 => {
+
+            },
+            _ => {
+                self.pop_context();
             }
         }
     }
 
     fn can_recover(&self, token: Token) -> bool {
-        return token.value == match self.context {
-            PContext::FnCall | PContext::Cast => ")",
-            PContext::FnDecl => "{",
+        return token.value == match self.curr_context.kind {
+            PContextKind::FnCall { .. } | PContextKind::Cast { .. } => ")",
+            PContextKind::FnDecl { .. } => "{",
             _ => ";"
         };
     }
